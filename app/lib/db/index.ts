16c81b195c932ebb4c7087d99b07834064aaa174
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-12-28 00:05:53 +0600

    Update index.ts

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -1,22 +1,11 @@
 // lib/db/index.ts
 
-import { ChatService } from '../types';
 import { firebaseService } from './firebase';
 import { supabaseService } from './supabase';
+import { ChatService } from '../types';
 
-const chatProvider = process.env.NEXT_PUBLIC_CHAT_PROVIDER;
+const provider = process.env.NEXT_PUBLIC_CHAT_PROVIDER || 'firebase';
 
-let dbService: ChatService;
+export const chatService: ChatService = provider === 'supabase' ? supabaseService : firebaseService;
 
-if (chatProvider === 'supabase') {
-  console.log("Using Supabase as a backend.");
-  dbService = supabaseService;
-} else if (chatProvider === 'firebase') {
-  console.log("Using Firebase as a backend.");
-  dbService = firebaseService;
-} else {
-  console.log("No chat provider specified, defaulting to Supabase.");
-  dbService = supabaseService;
-}
-
-export { dbService };
+export * from '../types';
