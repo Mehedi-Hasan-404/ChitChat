27ae349fb04dce2d01df13ab2a334a8a7a926f76
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-12-28 00:32:37 +0600

    Update useChat.ts

--- app/lib/hooks/useChat.ts
+++ app/lib/hooks/useChat.ts
@@ -1,12 +1,4 @@
-// lib/hooks/useChat.ts
+// app/lib/hooks/useChat.ts
 
-import { useContext } from 'react';
-import { ChatContext } from '@/components/providers/ChatProvider';
-
-export const useChat = () => {
-  const context = useContext(ChatContext);
-  if (!context) {
-    throw new Error('useChat must be used within a ChatProvider');
-  }
-  return context;
-};
+// Re-export useChat from ChatProvider
+export { useChat } from '../../components/providers/ChatProvider';
